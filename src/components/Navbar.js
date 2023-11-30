935f91e6549929a5197eb892f5ba6be2842c36e8
deepakgulati3104 <gulatideepak3104@gmail.com>
2023-11-30 10:21:43 +0530

    Repo added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -104,7 +104,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/DeepakGulati31/Capstone2"
+                href="https://github.com/DeepakGulati31/Deepak-Portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
